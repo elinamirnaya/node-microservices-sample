6c2193c56f3f2dcaed211f1b9bf7a0893e8d71d0
Elina Samoilenko <elina.samoilenko@epam.com>
2022-09-20 01:23:48 +0300

    add /distance with external data fetching

--- api/controller.js
+++ api/controller.js
@@ -1,8 +1,18 @@
 const furniture = require('../databases/furniture.json');
+const distance = require('../services/distance');
 
 var controller = {
     getFurniture: function(_req, res) {
         res.status(200).json(furniture);
+    },
+    getDistance: function(req, res) {   
+        distance.find(req, res, function(err, dist) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(dist);
+            }
+        });
     },
 };
 
--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const port = process.env.PORT || 3000;
 const controller = require('./api/controller');
 
 app.get('/', (_req, res) => {
-    res.send('Welcome! Please navigate to /furniture to get your data.');
+    res.send('Welcome! Please navigate to /furniture or /distance/:zipcode1/:zipcode2 to get your data.');
 });
 
 app.get('/furniture', controller.getFurniture);
+
+app.get('/distance/:zipcode1/:zipcode2', controller.getDistance);
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
